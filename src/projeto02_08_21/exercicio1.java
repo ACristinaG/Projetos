b2b5b0bb8004042ee83ca5dd826ee7af70a51424
Ana Cristina <anagoomez11@outlook.com>
2021-08-12 13:23:42 -0300

    Adicionando projeto do dia 02

---
+++ src/projeto02_08_21/exercicio1.java
@@ -0,0 +1,31 @@
+package projeto02_08_21;
+
+import java.util.Scanner;
+
+public class exercicio1 {
+
+public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		int anos, meses, dias, idade;
+		
+		System.out.println("Digite sua idade em anos: ");
+		anos = entrada.nextInt();
+		
+		System.out.println("E quantos meses? : ");
+		meses = entrada.nextInt();
+		
+		System.out.println("E quantos dias? : ");
+		dias = entrada.nextInt();
+		
+		anos = anos * 365;
+		meses = meses * 30;
+		idade = anos + meses + dias;
+		
+		System.out.println("A sua idade em dias é: " + idade);
+		
+		entrada.close();
+	}
+
+}
