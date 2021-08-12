69e60eb2fbaa81c2e53ce283bf5e7befeb949cab
Ana Cristina <anagoomez11@outlook.com>
2021-08-12 12:23:45 -0300

    Adicionando projeto do dia 09

---
+++ src/projeto09_08_21/cachorro.java
@@ -0,0 +1,30 @@
+package projeto09_08_21;
+
+public class cachorro extends Animal{
+	private String correr;
+
+
+
+	public cachorro (String nome, String raça, int idade, String som, String correr) {
+		super(nome, raça, idade, som);
+		this.setCorrer(correr);
+
+
+
+	}
+
+
+	public String getCorrer() {
+		return correr;
+	}
+
+
+	public void setCorrer(String correr) {
+		this.correr = correr;
+	}
+
+
+
+
+}
+
---
+++ src/projeto09_08_21/cavalo.java
@@ -0,0 +1,25 @@
+package projeto09_08_21;
+
+public class cavalo extends Animal {
+
+private String correr;
+
+
+public cavalo (String nome, String raça, int idade, String som, String correr) {
+	super(nome, raça, idade, som);
+	this.setCorrer(correr);
+
+}
+
+public String getCorrer() {
+	return correr;
+}
+
+public void setCorrer(String correr) {
+	this.correr = correr;
+}
+
+
+
+
+}
